8cf5e1a33407390e9966ad5670a6421bd13fe367
Wes Hampson <thehambone93@gmail.com>
2017-06-11 15:53:03 -0700

    Test primitives and pointers

--- Implementations/CSharp/WHampson.BFT/Program.cs
+++ Implementations/CSharp/WHampson.BFT/Program.cs
@@ -22,18 +22,109 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace WHampson.BFT
 {
+    public class Vect3D
+    {
+        public Pointer<Float> X { get; set; }
+        public Pointer<Float> Y { get; set; }
+        public Pointer<Float> Z { get; set; }
+    }
+
+    public class PlayerInfo
+    {
+        public Pointer<Int8> Health { get; set; }
+        public Pointer<Int8> Armor { get; set; }
+        public Pointer<Int32> Money { get; set; }
+        public Vect3D Location { get; set; }        // Nested structure
+        public Pointer<Int32>[] WeaponAmmo { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            // Allocate fixed memory on heap
+            IntPtr ptr = Marshal.AllocHGlobal(128);
+
+            // Fill buffer with test data
+            CreateTestData(ptr);
+
+            // Map structure to test memory (ugly... I know)
+            // This will later be done with template processor
+            PlayerInfo info = new PlayerInfo();
+            IntPtr mark = ptr;
+            info.Health = mark + 0 * Marshal.SizeOf(typeof(Int8));
+            info.Armor = mark + 1 * Marshal.SizeOf(typeof(Int8));
+            mark = info.Armor.Address + 3;
+            info.Money = mark + 0 * Marshal.SizeOf(typeof(Int32));
+            mark = info.Money.Address + Marshal.SizeOf(typeof(Int32));
+            info.Location = new Vect3D();
+            info.Location.X = mark + 0 * Marshal.SizeOf(typeof(Float));
+            info.Location.Y = mark + 1 * Marshal.SizeOf(typeof(Float));
+            info.Location.Z = mark + 2 * Marshal.SizeOf(typeof(Float));
+            mark = info.Location.Z.Address + Marshal.SizeOf(typeof(Float));
+            info.WeaponAmmo = new Pointer<Int32>[13];
+            for (int i = 0; i < info.WeaponAmmo.Length; i++)
+            {
+                info.WeaponAmmo[i] = mark + i * Marshal.SizeOf(typeof(Int32));
+            }
+
+            // Test modifying data through pointer
+            info.Health.Value = 50;
+            Console.WriteLine("Health:\t\t" + info.Health.Value);
+            
+            // Test modifying data directly
+            unsafe
+            {
+                byte* pHealth = (byte*) ptr;
+                *pHealth = 100;             // Health
+                *(pHealth + 1) = 125;       // Armor
+            }
+
+            // Print all data
+            Console.WriteLine("Health:\t\t" + info.Health.Value);
+            Console.WriteLine("Armor:\t\t" + info.Armor.Value);
+            Console.WriteLine("Money:\t\t" + info.Money.Value);
+            Console.WriteLine("Location.X:\t" + info.Location.X.Value);
+            Console.WriteLine("Location.Y:\t" + info.Location.Y.Value);
+            Console.WriteLine("Location.Z:\t" + info.Location.Z.Value);
+            for (int i = 0; i < info.WeaponAmmo.Length; i++)
+            {
+                Console.WriteLine("WeaponAmmo[" + i + "]:\t" + info.WeaponAmmo[i].Value);
+            }
+
+            // Free heap-allocated memory
+            Marshal.FreeHGlobal(ptr);
+
+            // Pause
+            Console.ReadKey();
+        }
+
+        static void CreateTestData(IntPtr ptr)
+        {
+            byte[] data = { 0x61, 0x45, 0x00, 0x00, // Health (97), Armor (69), (align 2 bytes)
+                            0xF2, 0xBE, 0x65, 0x01, // Money (23445234)
+                            0x48, 0x61, 0x28, 0x44, // Location.X (673.52)
+                            0x3D, 0xC2, 0xA7, 0xC4, // Location.Y (-1342.07)
+                            0x71, 0x3D, 0xC0, 0x41, // Location.Z (24.03)
+                            0x01, 0x00, 0x00, 0x00, // WeaponAmmo[0] (Fist) (1)
+                            0x01, 0x00, 0x00, 0x00, // WeaponAmmo[1] (Bat) (1)
+                            0x15, 0x16, 0x00, 0x00, // WeaponAmmo[2] (Pistol) (5653)
+                            0xE3, 0x24, 0x00, 0x00, // WeaponAmmo[3] (Uzi) (9443)
+                            0x1B, 0x01, 0x00, 0x00, // WeaponAmmo[4] (Shotgun) (283)
+                            0x38, 0x15, 0x00, 0x00, // WeaponAmmo[5] (AK) (5432)
+                            0x2A, 0x19, 0x00, 0x00, // WeaponAmmo[6] (M16) (6442)
+                            0x14, 0x02, 0x00, 0x00, // WeaponAmmo[7] (Sniper) (532)
+                            0x53, 0x00, 0x00, 0x00, // WeaponAmmo[8] (Rocket Launcher) (83)
+                            0x8A, 0x10, 0x00, 0x00, // WeaponAmmo[9] (Flamethrower) (4234)
+                            0x0A, 0x00, 0x00, 0x00, // WeaponAmmo[10] (Molotov) (10)
+                            0x2B, 0x00, 0x00, 0x00, // WeaponAmmo[11] (Grenades) (43)
+                            0x00, 0x00, 0x00, 0x00  // WeaponAmmo[12] (Detonator) (0)
+            };
+            Marshal.Copy(data, 0, ptr, data.Length);
         }
     }
 }
