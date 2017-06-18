cb7b6521e4b8404c572d449575fd63c88a0f262b
Wes Hampson <thehambone93@gmail.com>
2017-06-18 12:41:20 -0700

    Add utility class for Xml-related tasks

---
+++ Implementations/CSharp/WHampson.BFT/XmlUtils.cs
@@ -0,0 +1,45 @@
+﻿#region License
+/* Copyright (c) 2017 Wes Hampson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WHampson.BFT
+{
+    internal static class XmlUtils
+    {
+        public static string BuildXmlErrorMsg(XObject o, string msgFmt, params object[] fmtArgs)
+        {
+            IXmlLineInfo lineInfo = o;
+            string msg = string.Format(msgFmt, fmtArgs);
+            if (!msg.EndsWith("."))
+            {
+                msg += ".";
+            }
+
+            msg += " " + string.Format(" Line {0}, position {1}.", lineInfo.LineNumber, lineInfo.LinePosition);
+
+            return msg;
+        }
+    }
+}
