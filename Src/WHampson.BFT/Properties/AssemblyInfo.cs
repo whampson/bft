86dc345f866e98d879cc54b6b0f7b5298a80d83f
Wes Hampson <thehambone93@gmail.com>
2017-07-08 17:34:23 -0700

    Bump up to 0.3.0

--- Src/WHampson.BFT/Properties/AssemblyInfo.cs
+++ Src/WHampson.BFT/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2cc76928-f34e-4a9b-8623-1ccc543005c0")]
 
-[assembly: AssemblyFileVersion("0.2.0")]
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyInformationalVersion("0.3.0")]
