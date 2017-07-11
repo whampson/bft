e1083304043ac564b35d40a09eb4f7df15004672
Wes Hampson <thehambone93@gmail.com>
2017-07-10 23:39:19 -0700

    Add keyword constants and string-to-keyword map

--- Src/WHampson.Bft/Keywords.cs
+++ Src/WHampson.Bft/Keywords.cs
@@ -63,8 +63,8 @@ namespace WHampson.Bft
         public static readonly Keyword UInt16 = new Keyword(UInt16Identifier);
         public static readonly Keyword UInt32 = new Keyword(UInt32Identifier);
         public static readonly Keyword UInt64 = new Keyword(UInt64Identifier);
-        public static readonly Keyword ULong = new Keyword(ULong);
-        public static readonly Keyword UShort = new Keyword(UShort);
+        public static readonly Keyword ULong = new Keyword(ULongIdentifier);
+        public static readonly Keyword UShort = new Keyword(UShortIdentifier);
 
         // Directives
         public static readonly Keyword Align = new Keyword(AlignIdentifier);
@@ -75,6 +75,7 @@ namespace WHampson.Bft
         public static readonly Keyword Comment = new Keyword(CommentIdentifier);
         public static readonly Keyword Count = new Keyword(CountIdentifier);
         public static readonly Keyword Kind = new Keyword(KindIdentifier);
+        public static readonly Keyword Message = new Keyword(MessageIdentifier);
         public static readonly Keyword Name = new Keyword(NameIdentifier);
         public static readonly Keyword Typename = new Keyword(TypenameIdentifier);
 
@@ -119,6 +120,7 @@ namespace WHampson.Bft
             { CommentIdentifier, Comment },
             { CountIdentifier, Count },
             { KindIdentifier, Kind },
+            { MessageIdentifier, Message },
             { NameIdentifier, Name },
             { TypenameIdentifier, Typename }
         };
@@ -169,115 +171,8 @@ namespace WHampson.Bft
         private const string CommentIdentifier = "comment";
         private const string CountIdentifier = "count";
         private const string KindIdentifier = "kind";
+        private const string MessageIdentifier = "message";
         private const string NameIdentifier = "name";
         private const string TypenameIdentifier = "typename";
-
-
-
-
-
-        //// Builtin types
-        //public const string Byte = "byte";
-        //public const string Double = "double";
-        //public const string Dword = "dword";
-        //public const string Float = "float";
-        //public const string Int8 = "int8";
-        //public const string Int16 = "int16";
-        //public const string Int32 = "int32";
-        //public const string Int64 = "int64";
-        //public const string Qword = "qword";
-        //public const string Struct = "struct";
-        //public const string UInt8 = "uint8";
-        //public const string UInt16 = "uint16";
-        //public const string UInt32 = "uint32";
-        //public const string UInt64 = "uint64";
-        //public const string Word = "word";
-
-        //// Directives
-        //public const string Align = "align";
-        //public const string Echo = "echo";
-        //public const string Typedef = "typedef";
-
-        //// Modifiers
-        //public const string Comment = "comment";
-        //public const string Count = "count";
-        //public const string Kind = "kind";
-        //public const string Message = "message";
-        //public const string Name = "name";
-        ////public const string SentinelIdentifier = "sentinel";
-        ////public const string ThreshIdentifier = "thresh";
-        //public const string Typename = "typename";
-
-        //public enum BuiltinTypeId
-        //{
-        //    Double,
-        //    Float,
-        //    Int8,
-        //    Int16,
-        //    Int32,
-        //    Int64,
-        //    Struct,
-        //    UInt8,
-        //    UInt16,
-        //    UInt32,
-        //    UInt64
-        //}
-
-        //public enum DirectiveId
-        //{
-        //    Align,
-        //    Echo,
-        //    Typedef
-        //}
-
-        //public enum ModifierId
-        //{
-        //    Comment,
-        //    Count,
-        //    Kind,
-        //    Message,
-        //    Name,
-        //    //Sentinel,
-        //    //Thresh,
-        //    Typename
-        //}
-
-        //public static readonly Dictionary<string, BuiltinTypeId> BuiltinTypeIdMap = new Dictionary<string, BuiltinTypeId>()
-        //{
-        //    { Byte, BuiltinTypeId.UInt8 },
-        //    { Double, BuiltinTypeId.Double },
-        //    { Dword, BuiltinTypeId.UInt32 },
-        //    { Float, BuiltinTypeId.Float },
-        //    { Int8, BuiltinTypeId.Int8 },
-        //    { Int16, BuiltinTypeId.Int16 },
-        //    { Int32, BuiltinTypeId.Int32 },
-        //    { Int64, BuiltinTypeId.Int64 },
-        //    { Qword, BuiltinTypeId.UInt64 },
-        //    { Struct, BuiltinTypeId.Struct },
-        //    { UInt8, BuiltinTypeId.UInt8 },
-        //    { UInt16, BuiltinTypeId.UInt16 },
-        //    { UInt32, BuiltinTypeId.UInt32 },
-        //    { UInt64, BuiltinTypeId.UInt64 },
-        //    { Word, BuiltinTypeId.UInt16 }
-        //};
-
-        //public static readonly Dictionary<string, DirectiveId> DirectiveIdMap = new Dictionary<string, DirectiveId>()
-        //{
-        //    { Align, DirectiveId.Align },
-        //    { Echo, DirectiveId.Echo },
-        //    { Typedef, DirectiveId.Typedef },
-        //};
-
-        //public static readonly Dictionary<string, ModifierId> ModifierIdMap = new Dictionary<string, ModifierId>()
-        //{
-        //    { Comment, ModifierId.Comment },
-        //    { Count, ModifierId.Count },
-        //    { Kind, ModifierId.Kind },
-        //    { Message, ModifierId.Message },
-        //    { Name, ModifierId.Name },
-        //    //{ SentinelIdentifier, Modifier.Sentinel },
-        //    //{ ThreshIdentifier, Modifier.Thresh },
-        //    { Typename, ModifierId.Typename },
-        //};
     }
 }
