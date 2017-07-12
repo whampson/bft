c0ba94f1639e90df126300aaae3b91ae61ef82d2
Wes Hampson <thehambone93@gmail.com>
2017-07-11 22:37:12 -0700

    Documentation; remove unused code

--- Src/WHampson.Bft/CustomTypeInfo.cs
+++ Src/WHampson.Bft/CustomTypeInfo.cs
@@ -31,32 +31,39 @@ namespace WHampson.Bft
 {
     internal class CustomTypeInfo
     {
-        public static CustomTypeInfo CreateStruct(/*string name, */IEnumerable<XElement> members, int size)
+        /// <summary>
+        /// Creates a new <see cref="CustomTypeInfo"/> object whose type
+        /// declaration defines a grouped list of variables to be collectivey
+        /// treated as one block of memory.
+        /// </summary>
+        /// <param name="members">
+        /// The XML elements that define the variables contained within the
+        /// struct.
+        /// </param>
+        /// <param name="size">
+        /// The size in bytes of the struct. Must be constant.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="CustomTypeInfo"/> object.
+        /// </returns>
+        public static CustomTypeInfo CreateStruct(IEnumerable<XElement> members, int size)
         {
-            return new CustomTypeInfo(/*name, */null, members, size);
-        }
-
-        public static CustomTypeInfo CreatePrimitive(/*string name, */Type baseType)
-        {
-            int siz = Marshal.SizeOf(baseType);
-            return new CustomTypeInfo(/*name, */baseType, new List<XElement>(), siz);
+            return new CustomTypeInfo(null, members, size);
         }
 
         /// <summary>
-        /// Creates a new <see cref="CustomTypeInfo"/> object.
+        /// 
         /// </summary>
-        /// <param name="kind">
-        /// The parent type.
-        /// </param>
-        /// <param name="members">
-        /// A list of member elements.
-        /// </param>
-        private CustomTypeInfo(/*string name, */Type baseType, IEnumerable<XElement> members, int size)
+        /// <param name="baseType"></param>
+        /// <returns></returns>
+        public static CustomTypeInfo CreatePrimitive(Type baseType)
         {
-            //if (name == null)
-            //{
-            //    throw new ArgumentNullException("name");
-            //}
+            int siz = Marshal.SizeOf(baseType);
+            return new CustomTypeInfo(baseType, new List<XElement>(), siz);
+        }
+
+        private CustomTypeInfo(Type baseType, IEnumerable<XElement> members, int size)
+        {
             if (members == null)
             {
                 throw new ArgumentNullException("members");
@@ -66,32 +73,47 @@ namespace WHampson.Bft
                 throw new ArgumentException("Size must be a non-negative integer.");
             }
 
-            //TypeName = name;
             BaseType = baseType;
             Members = new List<XElement>(members);
             Size = size;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this custom type is a struct.
+        /// </summary>
         public bool IsStruct
         {
             get { return BaseType == null && Members.Count() != 0; }
         }
 
+        /// <summary>
+        /// Gets the type from which this custom type is derived.
+        /// </summary>
+        /// <remarks>
+        /// If <see cref="IsStruct"/> is <code>True</code>, this value will be
+        /// <code>null</code>. This is because structs are a mixture of many
+        /// different types and do not necessarily stem from a specific type.
+        /// </remarks>
         public Type BaseType
         {
             get;
         }
 
         /// <summary>
-        /// Gets the list of XML elements that descend from this custom type.
+        /// Gets the list of XML elements that define the structure of this
+        /// custom type.
         /// </summary>
+        /// <remarks>
+        /// If <see cref="IsStruct"/> is <code>False</code>, this list will be
+        /// empty.
+        /// </remarks>
         public IEnumerable<XElement> Members
         {
             get;
         }
 
         /// <summary>
-        /// Gets the size in bytes that this custom type occupies.
+        /// Gets the size in bytes of this custom type.
         /// </summary>
         public int Size
         {
