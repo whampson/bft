6138dc36c555d0b4cffd9e55059b1718a714fddd
Wes Hampson <thehambone93@gmail.com>
2017-07-18 00:41:00 -0700

    Moved to separate file.

---
+++ Src/WHampson.Bft/SymbolTableEntry.cs
@@ -0,0 +1,84 @@
+﻿#region License
+/* Copyright (c) 2017 Wes Hampson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
+
+namespace WHampson.Bft
+{
+    internal sealed class SymbolTableEntry
+    {
+        private TypeInfo type;
+        private bool isTypeSet;
+
+        public SymbolTableEntry(TypeInfo type, int offset, SymbolTable child)
+        {
+            Type = type;
+            Offset = offset;
+            Child = child;
+        }
+
+        public TypeInfo Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                // Ensure type is set exactly once
+                if (!isTypeSet)
+                {
+                    type = value;
+                    if (value != null)
+                    {
+                        isTypeSet = true;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Cannot set Type as it has already been set.");
+                }
+            }
+        }
+
+        public int Offset
+        {
+            get;
+        }
+
+        public SymbolTable Child
+        {
+            get;
+        }
+
+        public override string ToString()
+        {
+            string s = "{";
+            s += string.Format("{0}: {1:X8}", Type.Type.Name, Offset);
+            s += "}";
+
+            return s;
+        }
+    }
+}
