99343a263e815fddfb603c8182794266e3ca0c7f
Wes Hampson <thehambone93@gmail.com>
2017-07-20 00:22:15 -0700

    Refactor to account for removal of 'Keyword'

--- Src/WHampson.Bft/Keywords.cs
+++ Src/WHampson.Bft/Keywords.cs
@@ -35,144 +35,100 @@ namespace WHampson.Bft
         // (u)short -> (u)int16
 
 
-
         // Root element
-        public static readonly Keyword Bft = new Keyword(BftIdentifier);
+        public const string BftRoot = "bft";
 
         // Data types
-        public static readonly Keyword Bool = new Keyword(BoolIdentifier);
-        public static readonly Keyword Bool8 = new Keyword(Bool8Identifier);
-        public static readonly Keyword Bool16 = new Keyword(Bool16Identifier);
-        public static readonly Keyword Bool32 = new Keyword(Bool32Identifier);
-        public static readonly Keyword Bool64 = new Keyword(Bool64Identifier);
-        public static readonly Keyword Byte = new Keyword(ByteIdentifier);
-        public static readonly Keyword Char = new Keyword(CharIdentifier);
-        public static readonly Keyword Double = new Keyword(DoubleIdentifier);
-        public static readonly Keyword Float = new Keyword(FloatIdentifier);
-        public static readonly Keyword Half = new Keyword(HalfIdentifier);
-        public static readonly Keyword Int = new Keyword(IntIdentifier);
-        public static readonly Keyword Int8 = new Keyword(Int8Identifier);
-        public static readonly Keyword Int16 = new Keyword(Int16Identifier);
-        public static readonly Keyword Int32 = new Keyword(Int32Identifier);
-        public static readonly Keyword Int64 = new Keyword(Int64Identifier);
-        public static readonly Keyword Long = new Keyword(LongIdentifier);
-        public static readonly Keyword Short = new Keyword(ShortIdentifier);
-        public static readonly Keyword Struct = new Keyword(StructIdentifier);
-        public static readonly Keyword UInt = new Keyword(UIntIdentifier);
-        public static readonly Keyword UInt8 = new Keyword(UInt8Identifier);
-        public static readonly Keyword UInt16 = new Keyword(UInt16Identifier);
-        public static readonly Keyword UInt32 = new Keyword(UInt32Identifier);
-        public static readonly Keyword UInt64 = new Keyword(UInt64Identifier);
-        public static readonly Keyword ULong = new Keyword(ULongIdentifier);
-        public static readonly Keyword UShort = new Keyword(UShortIdentifier);
+        public const string Bool = "bool";
+        public const string Bool8 = "bool8";
+        public const string Bool16 = "bool16";
+        public const string Bool32 = "bool32";
+        public const string Bool64 = "bool64";
+        public const string Byte = "byte";
+        public const string Char = "char";
+        //public const string Char8 = "char8";
+        //public const string Char16dentifier = "char16";
+        //public const string Char32 = "char32";
+        //public const string Char64 = "char64";
+        public const string Double = "double";
+        //public const string DWord = "dword";
+        public const string Float = "float";
+        public const string Half = "half";
+        public const string Int = "int";
+        public const string Int8 = "int8";
+        public const string Int16 = "int16";
+        public const string Int32 = "int32";
+        public const string Int64 = "int64";
+        public const string Long = "long";
+        //public const string QWord = "qword";
+        public const string Short = "short";
+        public const string Struct = "struct";
+        public const string UInt = "uint";
+        public const string UInt8 = "uint8";
+        public const string UInt16 = "uint16";
+        public const string UInt32 = "uint32";
+        public const string UInt64 = "uint64";
+        public const string ULong = "ulong";
+        public const string UShort = "ushort";
+        //public const string Word = "word";
 
         // Directives
-        public static readonly Keyword Align = new Keyword(AlignIdentifier);
-        public static readonly Keyword Echo = new Keyword(EchoIdentifier);
-        public static readonly Keyword Typedef = new Keyword(TypedefIdentifier);
+        public const string Align = "align";
+        public const string Echo = "echo";
+        public const string Typedef = "typedef";
 
         // Modifiers
-        public static readonly Keyword Comment = new Keyword(CommentIdentifier);
-        public static readonly Keyword Count = new Keyword(CountIdentifier);
-        public static readonly Keyword Kind = new Keyword(KindIdentifier);
-        public static readonly Keyword Message = new Keyword(MessageIdentifier);
-        public static readonly Keyword Name = new Keyword(NameIdentifier);
-        public static readonly Keyword Typename = new Keyword(TypenameIdentifier);
+        public const string Comment = "comment";
+        public const string Count = "count";
+        public const string Kind = "kind";
+        public const string Message = "message";
+        public const string Name = "name";
+        //public const string Typename = "typename";
 
-        public static readonly Dictionary<string, Keyword> KeywordMap = new Dictionary<string, Keyword>()
+        public static readonly IEnumerable<string> KeywordList = new List<string>()
         {
             // Root element
-            { BftIdentifier, Bft },
+            BftRoot,
 
             // Data types
-            { BoolIdentifier, Bool },
-            { Bool8Identifier, Bool8 },
-            { Bool16Identifier, Bool16 },
-            { Bool32Identifier, Bool32 },
-            { Bool64Identifier, Bool64 },
-            { ByteIdentifier, Byte },
-            { CharIdentifier, Char },
-            { DoubleIdentifier, Double },
-            { FloatIdentifier, Float },
-            { HalfIdentifier, Half },
-            { IntIdentifier, Int },
-            { Int8Identifier, Int8 },
-            { Int16Identifier, Int16 },
-            { Int32Identifier, Int32 },
-            { Int64Identifier, Int64 },
-            { LongIdentifier, Long },
-            { ShortIdentifier, Short },
-            { StructIdentifier, Struct },
-            { UIntIdentifier, UInt },
-            { UInt8Identifier, UInt8 },
-            { UInt16Identifier, UInt16 },
-            { UInt32Identifier, UInt32 },
-            { UInt64Identifier, UInt64 },
-            { ULongIdentifier, ULong },
-            { UShortIdentifier, UShort },
+            Bool,
+            Bool8,
+            Bool16,
+            Bool32,
+            Bool64,
+            Byte,
+            Char,
+            Double,
+            Float,
+            Half,
+            Int,
+            Int8,
+            Int16,
+            Int32,
+            Int64,
+            Long,
+            Short,
+            Struct,
+            UInt,
+            UInt8,
+            UInt16,
+            UInt32,
+            UInt64,
+            ULong,
+            UShort,
 
             // Directives
-            { AlignIdentifier, Align },
-            { EchoIdentifier, Echo },
-            { TypedefIdentifier, Typedef },
+            Align,
+            Echo,
+            Typedef,
 
             // Modifiers
-            { CommentIdentifier, Comment },
-            { CountIdentifier, Count },
-            { KindIdentifier, Kind },
-            { MessageIdentifier, Message },
-            { NameIdentifier, Name },
-            { TypenameIdentifier, Typename }
+            Comment,
+            Count,
+            Kind,
+            Message,
+            Name,
+            //Typename
         };
-
-        // Root element
-        private const string BftIdentifier = "bft";
-
-        // Data types
-        private const string BoolIdentifier = "bool";
-        private const string Bool8Identifier = "bool8";
-        private const string Bool16Identifier = "bool16";
-        private const string Bool32Identifier = "bool32";
-        private const string Bool64Identifier = "bool64";
-        private const string ByteIdentifier = "byte";
-        private const string CharIdentifier = "char";
-        //private const string Char8Identifier = "char8";
-        //private const string Char16dentifier = "char16";
-        //private const string Char32Identifier = "char32";
-        //private const string Char64Identifier = "char64";
-        private const string DoubleIdentifier = "double";
-        //private const string DWordIdentifier = "dword";
-        private const string FloatIdentifier = "float";
-        private const string HalfIdentifier = "half";
-        private const string IntIdentifier = "int";
-        private const string Int8Identifier = "int8";
-        private const string Int16Identifier = "int16";
-        private const string Int32Identifier = "int32";
-        private const string Int64Identifier = "int64";
-        private const string LongIdentifier = "long";
-        //private const string QWordIdentifier = "qword";
-        private const string ShortIdentifier = "short";
-        private const string StructIdentifier = "struct";
-        private const string UIntIdentifier = "uint";
-        private const string UInt8Identifier = "uint8";
-        private const string UInt16Identifier = "uint16";
-        private const string UInt32Identifier = "uint32";
-        private const string UInt64Identifier = "uint64";
-        private const string ULongIdentifier = "ulong";
-        private const string UShortIdentifier = "ushort";
-        //private const string WordIdentifier = "word";
-
-        // Directives
-        private const string AlignIdentifier = "align";
-        private const string EchoIdentifier = "echo";
-        private const string TypedefIdentifier = "typedef";
-
-        // Modifiers
-        private const string CommentIdentifier = "comment";
-        private const string CountIdentifier = "count";
-        private const string KindIdentifier = "kind";
-        private const string MessageIdentifier = "message";
-        private const string NameIdentifier = "name";
-        private const string TypenameIdentifier = "typename";
     }
 }
