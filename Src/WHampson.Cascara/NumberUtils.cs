26787213c9c3e1ca132393a5e1f8c4bea352e07f
Wes Hampson <thehambone93@gmail.com>
2017-07-21 22:33:34 -0700

    Remove TryParseInteger() as it will not be used.

--- Src/WHampson.Cascara/NumberUtils.cs
+++ Src/WHampson.Cascara/NumberUtils.cs
@@ -22,26 +22,15 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace WHampson.Cascara
 {
     internal static class NumberUtils
     {
-        private const string Base2Pattern = @"^([01]{1,64})[bB]$";
-        private const string Base8Pattern = @"^([0-7]{1,64})[oO]$";
-        private const string Base16Pattern = @"^0?[xX]{1}([0-9a-fA-F]{1,16})$|^([0-9a-fA-F]{1,16})[hH]{1}$";
         private const string MathExprPattern = @"^[-+*/().\d ]+$";
 
-        private static readonly Regex Base2Regex = new Regex(Base2Pattern);
-        private static readonly Regex Base8Regex = new Regex(Base8Pattern);
-        private static readonly Regex Base16Regex = new Regex(Base16Pattern);
         private static readonly Regex MathExprRegex = new Regex(MathExprPattern);
 
         public static double EvaluateExpression(string expr)
@@ -66,65 +55,6 @@ namespace WHampson.Cascara
         public static bool IsInteger(double d)
         {
             return Math.Abs(d % 1) <= (Double.Epsilon * 100);
-        }
-
-        /// <summary>
-        /// Attempts to convert a string to a 64-bit integer value.
-        /// A return value indicates whether the conversion succeeded.
-        /// </summary>
-        /// <remarks>
-        /// This method differs from <see cref="int.TryParse(string, out int)"/>
-        /// in that it will handle base-2, base-8, bbase-10 and base-16 number representations.
-        ///     Base-2  -- ends with 'b' or 'B' character
-        ///     Base-8  -- ends with 'o' or 'O' character
-        ///     Base-10 -- no prefix or suffix characters
-        ///     Base-16 -- starts with 'x', 'X', 0x', or '0X'; or ends with 'h' or 'H'
-        /// </remarks>
-        /// <param name="valStr">
-        /// The string representation of the number to convert.
-        /// </param>
-        /// <param name="val">
-        /// The converted number.
-        /// </param>
-        /// <returns>
-        /// <code>True</code> if the conversion was successful,
-        /// <code>False</code> otherwise.
-        /// </returns>
-        public static bool TryParseInteger(string valStr, out long val)
-        {
-            int radix = 10;
-            Match base2Match = Base2Regex.Match(valStr);
-            Match base8Match = Base8Regex.Match(valStr);
-            Match base16Match = Base16Regex.Match(valStr);
-
-            if (base2Match.Success)
-            {
-                valStr = base2Match.Groups[1].Value;
-                radix = 2;
-            }
-            else if (base8Match.Success)
-            {
-                valStr = base8Match.Groups[1].Value;
-                radix = 8;
-            }
-            else if (base16Match.Success)
-            {
-                Group g1 = base16Match.Groups[1];   // 0x*
-                Group g2 = base16Match.Groups[2];   // *h
-                valStr = (!string.IsNullOrWhiteSpace(g1.Value)) ? g1.Value : g2.Value;
-                radix = 16;
-            }
-
-            try
-            {
-                val = Convert.ToInt64(valStr, radix);
-                return true;
-            }
-            catch (FormatException e)
-            {
-                val = 0;
-                return false;
-            }
         }
     }
 }
