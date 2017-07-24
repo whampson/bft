cad6f59118a87e1291caf264cf9b83d60a00e9b3
Wes Hampson <thehambone93@gmail.com>
2017-07-23 21:04:36 -0700

    Add ICascaraType

---
+++ Src/WHampson.Cascara/Types/ICascaraType.cs
@@ -0,0 +1,47 @@
+﻿#region License
+/* Copyright (c) 2017 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+namespace WHampson.Cascara.Types
+{
+    /// <summary>
+    /// Provides a framework for data types used by Cascara.
+    /// </summary>
+    public interface ICascaraType
+    {
+        /// <summary>
+        /// Gets a copy of the bytes that make up the data this type represents.
+        /// </summary>
+        /// <returns>
+        /// A byte array containing this type instance's data.
+        /// </returns>
+        byte[] GetBytes();
+
+        /// <summary>
+        /// Gets the size in bytes of the data that this type represents.
+        /// </summary>
+        /// <returns>
+        /// The size of this data type in bytes.
+        /// </returns>
+        int GetSize();
+    }
+}
