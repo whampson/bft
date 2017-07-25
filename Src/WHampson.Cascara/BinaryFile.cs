a8578886cd809bb8c196c8926397c6008b465354
Wes Hampson <thehambone93@gmail.com>
2017-07-25 12:48:39 -0700

    Add 'BinaryFile' class

---
+++ Src/WHampson.Cascara/BinaryFile.cs
@@ -0,0 +1,142 @@
+﻿#region License
+/* Copyright (c) 2017 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace WHampson.Cascara
+{
+    public class BinaryFile : IDisposable
+    {
+        public static BinaryFile Open(string filePath)
+        {
+            const int OneGibiByte = 1 << 30;
+
+            FileInfo fInfo = new FileInfo(filePath);
+            if (fInfo.Length > OneGibiByte)
+            {
+                throw new IOException("File size must be less than 1 GiB.");
+            }
+
+            int len = (int) fInfo.Length;
+            byte[] data = new byte[len];
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                fs.Read(data, 0, len);
+            }
+
+            IntPtr dataPtr = Marshal.AllocHGlobal(len);
+            Marshal.Copy(data, 0, dataPtr, len);
+
+            return new BinaryFile(dataPtr, len);
+        }
+
+        private IntPtr dataPtr;
+        private int dataLen;
+
+        private bool hasBeenDisposed;
+
+        private BinaryFile(IntPtr addr, int len)
+        {
+            dataPtr = addr;
+            dataLen = len;
+            hasBeenDisposed = false;
+        }
+
+        public int Length
+        {
+            get
+            {
+                if (hasBeenDisposed)
+                {
+                    throw new ObjectDisposedException(GetType().FullName);
+                }
+
+                return dataLen;
+            }
+        }
+
+        public void Close()
+        {
+            if (hasBeenDisposed)
+            {
+                throw new ObjectDisposedException(GetType().FullName);
+            }
+
+            Dispose();
+        }
+
+        public void Write(string filePath)
+        {
+            if (hasBeenDisposed)
+            {
+                throw new ObjectDisposedException(GetType().FullName);
+            }
+
+            byte[] data = new byte[dataLen];
+            Marshal.Copy(dataPtr, data, 0, dataLen);
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                fs.Write(data, 0, dataLen);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!hasBeenDisposed)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // Free unmanaged resources (unmanaged objects)
+                if (dataPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(dataPtr);
+                    dataPtr = IntPtr.Zero;
+                }
+
+                hasBeenDisposed = true;
+            }
+        }
+
+        ~BinaryFile()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
