f8bea3fbff3e1aead9174644ce6f5106d7fc0433
Wes Hampson <thehambone93@gmail.com>
2017-07-29 18:03:12 -0700

    Add TypeUtils class for misc. functions related to Types

---
+++ Src/WHampson.Cascara/TypeUtils.cs
@@ -0,0 +1,62 @@
+﻿#region License
+/* Copyright (c) 2017 Wes Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHampson.Cascara
+{
+    internal static class TypeUtils
+    {
+        // Checks whether the given type stems from a generic type.
+        // Taken from https://stackoverflow.com/questions/74616#1075059
+        public static bool IsAssignableToGenericType(Type givenType, Type genericType)
+        {
+            Type[] interfaceTypes = givenType.GetInterfaces();
+
+            foreach (Type typ in interfaceTypes)
+            {
+                if (typ.IsGenericType && typ.GetGenericTypeDefinition() == genericType)
+                {
+                    return true;
+                }
+            }
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            Type baseType = givenType.BaseType;
+            if (baseType == null)
+            {
+                return false;
+            }
+
+            return IsAssignableToGenericType(baseType, genericType);
+        }
+    }
+}
