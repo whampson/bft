1f3fe19003d1a46ba2236a2a5c04abb4f92f5853
Wes Hampson <thehambone93@gmail.com>
2017-11-29 18:22:38 -0600

    Add Name property; code cleanup and documentation

--- Cascara/Src/LayoutFile.cs
+++ Cascara/Src/LayoutFile.cs
@@ -29,7 +29,7 @@ using System.Xml.Linq;
 namespace WHampson.Cascara
 {
     /// <summary>
-    /// Represents a Layout File, used for defining the structure of a binary file.
+    /// Represents a Layout File, which is used for defining the structure of a binary file.
     /// </summary>
     public sealed class LayoutFile
     {
@@ -44,99 +44,22 @@ namespace WHampson.Cascara
         /// from the provided file.
         /// </returns>
         /// <exception cref="ArgumentException">
-        /// Thrown if the XML path is invalid.
+        /// Thrown if the XML path is empty or null.
+        /// </exception>
+        /// <exception cref="System.IO.FileNotFoundException">
+        /// Thrown if the path provided does not exist.
         /// </exception>
         /// <exception cref="XmlException">
-        /// If an error occurs while loading the Layout XML file.
+        /// Thrown if there is an error while loading the XML document.
         /// </exception>
         public static LayoutFile Load(string layoutFilePath)
         {
-            LayoutFile lf = new LayoutFile(OpenXmlFile(layoutFilePath))
+            LayoutFile layout = new LayoutFile(OpenXmlFile(layoutFilePath))
             {
                 SourcePath = layoutFilePath
             };
 
-            return lf;
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="LayoutFile"/> object
-        /// from an XML data string.
-        /// </summary>
-        /// <param name="xmlData">
-        /// The contents of an XML file containing the layout information.
-        /// </param>
-        public LayoutFile(string xmlData)
-            : this(XDocument.Parse(xmlData))
-        { }
-
-        /// <summary>
-        /// Creates a new <see cref="LayoutFile"/> object
-        /// from an existing <see cref="XDocument"/> object.
-        /// </summary>
-        /// <param name="xDoc">
-        /// An <see cref="XDocument"/> object containing the
-        /// Layout File XML data.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        /// If the provided <see cref="XDocument"/> object is <code>null</code>.
-        /// </exception>
-        public LayoutFile(XDocument xDoc)
-        {
-            Document = xDoc ?? throw new ArgumentNullException("xDoc");
-            SourcePath = null;
-
-            ValidateRootElement();
-        }
-
-        public string SourcePath
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Gets the <see cref="XDocument"/> associated with
-        /// this <see cref="LayoutFile"/> file.
-        /// </summary>
-        internal XDocument Document
-        {
-            get;
-        }
-
-        /// <summary>
-        /// Ensures the root element of <see cref="Document"/> is correct.
-        /// </summary>
-        /// <exception cref="LayoutException">
-        /// If the root element is empty or has the wrong name.
-        /// </exception>
-        private void ValidateRootElement()
-        {
-            if (Document.Root.Name.LocalName != Keywords.DocumentRoot)
-            {
-                string fmt = "Layout Files must have a root element named '{0}'.";
-                throw LayoutException.Create(Document.Root, fmt, Keywords.DocumentRoot);
-            }
-            if (Document.Root.Elements().Count() == 0)
-            {
-                throw new LayoutException("Empty Layout File.");
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            return XNode.EqualityComparer.GetHashCode(Document);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is LayoutFile))
-            {
-                return false;
-            }
-
-            LayoutFile lf = (LayoutFile) obj;
-            return XNode.DeepEquals(Document, lf.Document);
+            return layout;
         }
 
         /// <summary>
@@ -149,7 +72,10 @@ namespace WHampson.Cascara
         /// The loaded XML data as an <see cref="XDocument"/>.
         /// </returns>
         /// <exception cref="ArgumentException">
-        /// Thrown if the XML path is invalid.
+        /// Thrown if the XML path is empty or null.
+        /// </exception>
+        /// <exception cref="System.IO.FileNotFoundException">
+        /// Thrown if the path provided does not exist.
         /// </exception>
         /// <exception cref="XmlException">
         /// Thrown if there is an error while loading the XML document.
@@ -158,11 +84,125 @@ namespace WHampson.Cascara
         {
             if (string.IsNullOrWhiteSpace(path))
             {
-                throw new ArgumentException("Path cannot be empty or null");
+                throw new ArgumentException("Path cannot be empty or null.", nameof(path));
             }
 
-
             return XDocument.Load(path, LoadOptions.SetLineInfo);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="LayoutFile"/> object from an XML data string.
+        /// </summary>
+        /// <param name="xmlData">
+        /// The contents of an XML file containing the layout information.
+        /// </param>
+        public LayoutFile(string xmlData)
+            : this(XDocument.Parse(xmlData))
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="LayoutFile"/> object from an existing
+        /// <see cref="XDocument"/> object.
+        /// </summary>
+        /// <param name="xDoc">
+        /// An <see cref="XDocument"/> object containing th Layout File XML data.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if the provided <see cref="XDocument"/> object is <code>Null</code>.
+        /// </exception>
+        public LayoutFile(XDocument xDoc)
+        {
+            Document = xDoc ?? throw new ArgumentNullException(nameof(xDoc));
+            ValidateRootElement();
+
+            SourcePath = null;
+            Name = Document.Root.Attribute(Keywords.Name).Value;
+        }
+
+        /// <summary>
+        /// Gets the name of this <see cref="LayoutFile"/>.
+        /// </summary>
+        public string Name
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the file path used to load the <see cref="LayoutFile"/>.
+        /// </summary>
+        public string SourcePath
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="XDocument"/> associated with this
+        /// <see cref="LayoutFile"/> file.
+        /// </summary>
+        internal XDocument Document
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Ensures the root element of <see cref="Document"/> is correct.
+        /// </summary>
+        /// <exception cref="LayoutException">
+        /// If the root element is empty, has the wrong name, or is missing
+        /// the 'name' attribute.
+        /// </exception>
+        private void ValidateRootElement()
+        {
+            if (Document.Root.Name.LocalName != Keywords.DocumentRoot)
+            {
+                string fmt = "Layout Files must have a root element named '{0}'.";
+                throw LayoutException.Create(this, Document.Root, fmt, Keywords.DocumentRoot);
+            }
+
+            if (Document.Root.Elements().Count() == 0)
+            {
+                throw new LayoutException(this, "Empty Layout File.");
+            }
+
+            if (Document.Root.Attribute(Keywords.Name) == null)
+            {
+                string fmt = "Missing required attribute '{0}'.";
+                throw LayoutException.Create(this, Document.Root, fmt, Keywords.Name);
+            }
+        }
+
+        /// <summary>
+        /// Returns a unique hash code of this object based on its contents.
+        /// </summary>
+        /// <returns>
+        /// A unique hash code of this object based on its contents.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return XNode.EqualityComparer.GetHashCode(Document);
+        }
+
+        /// <summary>
+        /// Compares an <see cref="object"/> against this <see cref="LayoutException"/>
+        /// for equality.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="object"/> to compare against.
+        /// </param>
+        /// <returns>
+        /// <code>True</code> if the objects are equal,
+        /// <code>False</code> otherwise
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LayoutFile))
+            {
+                return false;
+            }
+
+            return XNode.DeepEquals(Document, ((LayoutFile) obj).Document);
         }
     }
 }
