aa1ca418dd04bb1a591f8d53e1cc9e204ea2a0b3
Wes Hampson <thehambone93@gmail.com>
2017-11-30 21:13:14 -0600

    Begin refactor
    End goal: validate layout XML data in this class to make processing on binary data more straightforward

--- Cascara/Src/BinaryLayout.cs
+++ Cascara/Src/BinaryLayout.cs
@@ -22,6 +22,7 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
@@ -37,125 +38,111 @@ namespace WHampson.Cascara
     /// </remarks>
     public sealed class BinaryLayout
     {
-        /// <summary>
-        /// Creates a <see cref="BinaryLayout"/> object from the data stored in
-        /// the XML file located at the specified path.
-        /// </summary>
-        /// <param name="xmlPath">
-        /// The path to the XML file containing the binary layout information.
-        /// </param>
-        /// <returns>
-        /// A <see cref="BinaryLayout"/> object containing the layout information
-        /// for a specific binary file format.
-        /// </returns>
-        /// <exception cref="ArgumentException">
-        /// Thrown if the XML path is empty or null.
-        /// </exception>
-        /// <exception cref="FileNotFoundException">
-        /// Thrown if the path provided does not exist.
-        /// </exception>
-        /// <exception cref="XmlException">
-        /// Thrown if there is an error while loading the XML document.
-        /// </exception>
-        /// <exception cref="LayoutException">
-        /// Thrown if the binary layout XML data contains an error.
-        /// </exception>
         public static BinaryLayout Load(string xmlPath)
         {
             if (string.IsNullOrWhiteSpace(xmlPath))
             {
                 throw new ArgumentException("Path cannot be empty or null.", nameof(xmlPath));
             }
-            XDocument doc = XDocument.Load(xmlPath, LoadOptions.SetLineInfo);
-            
-            return new BinaryLayout(doc) { SourcePath = xmlPath };
+
+            try
+            {
+                XDocument doc = XDocument.Load(xmlPath, LoadOptions.SetLineInfo);
+                return new BinaryLayout(doc, xmlPath);
+            }
+            catch (Exception e)
+            {
+                throw LayoutException.Create(null, e, null, "Failed to load layout.");
+            }
         }
 
-        /// <summary>
-        /// Creates a new <see cref="BinaryLayout"/> object from an XML data string.
-        /// </summary>
-        /// <param name="xmlData">
-        /// An XML string containing the binary layout information.
-        /// </param>
-        /// <exception cref="XmlException">
-        /// Thrown if there is an error while loading the XML document.
-        /// </exception>
-        /// <exception cref="LayoutException">
-        /// Thrown if the binary layout XML data contains an error.
-        /// </exception>
+        private Dictionary<string, BinaryLayout> includedLayouts;
+        private Dictionary<string, double> locals;
+        private Stack<Symbol> symbolStack;
+
         public BinaryLayout(string xmlData)
             : this(XDocument.Parse(xmlData))
         {
         }
 
-        /// <summary>
-        /// Creates a new <see cref="BinaryLayout"/> object from the XML data
-        /// contained within an <see cref="XDocument"/>.
-        /// </summary>
-        /// <param name="xDoc">
-        /// An <see cref="XDocument"/> containing the binary layout XML data.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        /// Thrown if the provided <see cref="XDocument"/> object is <code>null</code>.
-        /// </exception>
-        /// <exception cref="LayoutException">
-        /// Thrown if the binary layout XML data contains an error.
-        /// </exception>
         public BinaryLayout(XDocument xDoc)
+            : this(xDoc, null)
+        {
+        }
+
+        private BinaryLayout(XDocument xDoc, string sourcePath)
         {
             Document = xDoc ?? throw new ArgumentNullException(nameof(xDoc));
             ValidateRootElement();
 
-            SourcePath = null;
+            includedLayouts = new Dictionary<string, BinaryLayout>();
+            locals = new Dictionary<string, double>();
+            symbolStack = new Stack<Symbol>();
+
+            SourcePath = sourcePath;
             Name = Document.Root.Attribute(Keywords.Name).Value;
+            UserDefinedTypes = new Dictionary<string, TypeDefinition>();
 
             // TODO: Check entire layout for invalid names and syntax errors
+
+            try
+            {
+                AnalyzeLayout(Document);
+            }
+            catch (Exception e)
+            {
+                throw LayoutException.Create(null, e, null, "Layout validation failure.");
+            }
         }
 
-        /// <summary>
-        /// Gets the name of this <see cref="BinaryLayout"/>.
-        /// </summary>
+        public string this[string key]
+        {
+            get
+            {
+                XAttribute attr;
+                if ((attr = Document.Root.Attribute(key)) == null)
+                {
+                    return "";
+                }
+                else
+                {
+                    return attr.Value;
+                }
+            }
+        }
+
         public string Name
         {
             get;
         }
 
-        /// <summary>
-        /// Gets the file path used to load this <see cref="BinaryLayout"/>.
-        /// Returns <code>null</code> if the Layout File was not loaded 
-        /// </summary>
         public string SourcePath
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the <see cref="XDocument"/> associated with this <see cref="BinaryLayout"/>.
-        /// </summary>
         internal XDocument Document
         {
             get;
         }
 
-        /// <summary>
-        /// Ensures the root element of <see cref="Document"/> is correct.
-        /// </summary>
-        /// <exception cref="LayoutException">
-        /// If the root element is empty, has the wrong name, or is missing
-        /// the 'name' attribute.
-        /// </exception>
+        internal Dictionary<string, TypeDefinition> UserDefinedTypes
+        {
+            get;
+        }
+
+        private Symbol CurrentSymbolTable
+        {
+            get { return symbolStack.Peek(); }
+        }
+
         private void ValidateRootElement()
         {
             if (Document.Root.Name.LocalName != Keywords.DocumentRoot)
             {
-                string fmt = "Layout Files must have a root element named '{0}'.";
+                string fmt = "Must have a root element named '{0}'.";
                 throw LayoutException.Create(this, Document.Root, fmt, Keywords.DocumentRoot);
-            }
-
-            if (Document.Root.Elements().Count() == 0)
-            {
-                throw new LayoutException(this, "Empty Layout File.");
             }
 
             if (Document.Root.Attribute(Keywords.Name) == null)
@@ -163,6 +150,160 @@ namespace WHampson.Cascara
                 string fmt = "Missing required attribute '{0}'.";
                 throw LayoutException.Create(this, Document.Root, fmt, Keywords.Name);
             }
+
+            if (Document.Root.Elements().Count() == 0)
+            {
+                throw LayoutException.Create(this, Document.Root, "Empty layout.");
+            }
+        }
+
+        private void AnalyzeLayout(XDocument doc)
+        {
+            //ValidateRootElement();
+
+            // TODO: handle 'include' cycles
+
+            symbolStack.Push(Symbol.CreateRootSymbol());
+            AnalyzeStructMembers(doc.Root);
+        }
+
+        private void AnalyzeElement(XElement elem)
+        {
+            string elemName = elem.Name.LocalName;
+
+            // Analyze 'struct' and 'union'
+            if (elemName == Keywords.Struct || elemName == Keywords.Union)
+            {
+                AnalyzeStruct(elem);
+                return;
+            }
+
+            // Ensure element name corresponds to either a primitive type,
+            // user-defined type, or directive
+            bool isUserDefinedType = UserDefinedTypes.TryGetValue(elemName, out TypeDefinition typeDef);
+            bool isDirective = Keywords.Directives.ContainsKey(elemName);
+            if (!isUserDefinedType && !isDirective)
+            {
+                string fmt = "Unknown type or directive '{0}'.";
+                throw LayoutException.Create(this, elem, fmt, elemName);
+            }
+
+            if (isDirective)
+            {
+                // AnalyzeDirective(elem);
+                return;
+            }
+
+            // Ensure element has no child elements (only allowed on structures)
+            if (HasChildren(elem))
+            {
+                string fmt = "Type '{0}' cannot contain child elements.";
+                throw LayoutException.Create(this, elem, fmt, elemName);
+            }
+
+            if (isUserDefinedType && typeDef.IsStruct)
+            {
+                // Analuze user-defined structure type
+                // "Copy and paste" members from type definition into copy of current element
+                XElement copy = new XElement(elem);
+                copy.Add(typeDef.Members);
+                AnalyzeStruct(copy);
+            }
+            else
+            {
+                AnalyzePrimitiveType(elem);
+            }
+        }
+
+        private void AnalyzeStruct(XElement elem)
+        {
+            AnalyzeAttributes(elem, Keywords.Comment, Keywords.Count, Keywords.Name);
+
+            string name = null;
+            if (elem.Attribute(Keywords.Name) != null)
+            {
+                name = elem.Attribute(Keywords.Name).Value;
+            }
+
+            Symbol newTable;
+            if (name != null)
+            {
+                if (IsDefined(name))
+                {
+                    string fmt = "Variable '{0}' previously defined.";
+                    throw LayoutException.Create(this, elem, fmt, name);
+                }
+
+                newTable = CurrentSymbolTable.Insert(name);
+            }
+            else
+            {
+                newTable = Symbol.CreateNamelessSymbol(CurrentSymbolTable);
+            }
+
+            symbolStack.Push(newTable);
+            AnalyzeStructMembers(elem);
+            symbolStack.Pop();
+        }
+
+        private void AnalyzeStructMembers(XElement elem)
+        {
+            foreach (XElement memberElem in elem.Elements())
+            {
+                AnalyzeElement(memberElem);
+            }
+        }
+
+        private void AnalyzePrimitiveType(XElement elem)
+        {
+            AnalyzeAttributes(elem, Keywords.Comment, Keywords.Count, Keywords.Name);
+
+            string name = null;
+            if (elem.Attribute(Keywords.Name) != null)
+            {
+                name = elem.Attribute(Keywords.Name).Value;
+            }
+
+            if (name != null)
+            {
+                if (IsDefined(name))
+                {
+                    string fmt = "Variable '{0}' previously defined.";
+                    throw LayoutException.Create(this, elem, fmt, name);
+                }
+
+                CurrentSymbolTable.Insert(name);
+            }
+        }
+
+        private void AnalyzeAttributes(XElement elem, params string[] validAttributes)
+        {
+            foreach (XAttribute attr in elem.Attributes())
+            {
+                string name = attr.Name.LocalName;
+                if (!validAttributes.Contains(name))
+                {
+                    string fmt = "Unknown attribute '{0}'.";
+                    throw LayoutException.Create(this, attr, fmt, name);
+                }
+                else if (string.IsNullOrWhiteSpace(attr.Value))
+                {
+                    string fmt = "Attribute '{0}' cannot have an empty value.";
+                    throw LayoutException.Create(this, attr, fmt, name);
+                }
+
+                // TODO: ensure attribute values have the correct type snd varnames are correct
+            }
+        }
+
+        private bool IsDefined(string variableName)
+        {
+            return locals.ContainsKey(variableName) || CurrentSymbolTable.Contains(variableName);
+        }
+
+        private bool HasChildren(XElement elem)
+        {
+            return elem != null && elem.Elements().Count() != 0;
         }
 
         /// <summary>
@@ -173,7 +314,8 @@ namespace WHampson.Cascara
         /// </returns>
         public override int GetHashCode()
         {
-            return XNode.EqualityComparer.GetHashCode(Document);
+            return Name.GetHashCode() * 17
+                + XNode.EqualityComparer.GetHashCode(Document);
         }
 
         /// <summary>
@@ -189,12 +331,21 @@ namespace WHampson.Cascara
         /// </returns>
         public override bool Equals(object obj)
         {
+            // TODO: LONG TERM: define equality as describing
+            // the same file structure, not as having the same XML contents
+            // To do this, verify equality of:
+            //   - symbols
+            //   - user-defined types
+            //   - min. size of data being described
+
             if (!(obj is BinaryLayout))
             {
                 return false;
             }
+            BinaryLayout other = (BinaryLayout) obj;
 
-            return XNode.DeepEquals(Document, ((BinaryLayout) obj).Document);
+            return Name == other.Name
+                && XNode.DeepEquals(Document, other.Document);
         }
     }
 }
