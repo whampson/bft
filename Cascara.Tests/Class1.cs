a94f4dddceaed09f82f2c0d5c48141c5b8be5738
Wes Hampson <thehambone93@gmail.com>
2017-12-23 18:48:26 -0600

    Add NUnit test project

---
+++ Cascara.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using WHampson.Cascara;
+
+namespace WHampson.Cascara.Tests
+{
+    [TestFixture]
+    public class BinaryLayoutTests
+    {
+        [Test]
+        public void TestTest()
+        {
+            // Arrange
+            string xmlData = "<cascaraLayout name='Foobar'></cascaraLayout>";
+
+            // Act/assert
+            Assert.That(() => BinaryLayout.Create(xmlData),
+                Throws.TypeOf<LayoutException>()
+                .With.Message.EqualTo("Empty layout."));
+        }
+    }
+}
