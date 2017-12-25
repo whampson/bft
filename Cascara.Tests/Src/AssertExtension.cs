3fb5c2a35a77af6c4e232ee342cd716b0170648a
Wes Hampson <thehambone93@gmail.com>
2017-12-25 01:11:27 -0600

    Add 'ThrowsWithMessage'

---
+++ Cascara.Tests/Src/AssertExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Cascara.Tests
+{
+    public class AssertExtension : Assert
+    {
+        public static T ThrowsWithMessage<T>(Func<object> testCode, string message)
+            where T : Exception
+        {
+            var ex = Assert.Throws<T>(testCode);
+            Assert.Equal(ex.Message, message);
+
+            return ex;
+        }
+    }
+}
