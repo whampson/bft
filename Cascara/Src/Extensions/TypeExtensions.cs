79c5f70da937bba11223342b34285d58158f9270
Wes Hampson <thehambone93@gmail.com>
2017-12-27 17:14:21 -0600

    Add GetPublicConstants()

---
+++ Cascara/Src/Extensions/TypeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WHampson.Cascara
+{
+    /// <summary>
+    /// Extension methods for the <see cref="System.Type"/> class.
+    /// </summary>
+    internal static class TypeExtensions
+    {
+        /// <summary>
+        /// Gets the values of all public constants of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the constants to get.</typeparam>
+        /// <param name="clazz">The class to get constants from.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> of the values of all constants.</returns>
+        public static IEnumerable<T> GetPublicConstants<T>(this Type clazz)
+            where T : IConvertible
+        {
+            if (clazz == null)
+            {
+                throw new ArgumentNullException(nameof(clazz));
+            }
+
+            return clazz.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Where(f => f.IsLiteral && !f.IsInitOnly && f.FieldType == typeof(T))
+                .Select(f => (T) f.GetRawConstantValue())
+                .Concat(clazz.GetNestedTypes(BindingFlags.Public).SelectMany(GetPublicConstants<T>));
+        }
+    }
+}
