13c4520c80af22bfa58916919f2e0eda9e0ae509
Wes Hampson <thehambone93@gmail.com>
2017-12-27 21:10:44 -0600

    Add ThrowsWithMessage() overload with format string

--- Cascara.Tests/Src/AssertExtension.cs
+++ Cascara.Tests/Src/AssertExtension.cs
@@ -13,5 +13,16 @@ namespace Cascara.Tests
 
             return ex;
         }
+
+        public static T ThrowsWithMessage<T>(Func<object> testCode, string fmt, params object[] args)
+            where T : Exception
+        {
+            string message = string.Format(fmt, args);
+
+            var ex = Assert.Throws<T>(testCode);
+            Assert.Equal(ex.Message, message);
+
+            return ex;
+        }
     }
 }
