03923d622b7465e54c78dc6f0314a6c80f9d9779
Wes Hampson <thehambone93@gmail.com>
2017-12-27 22:20:47 -0600

    Add statement tests

---
+++ Cascara.Tests/Src/StatementTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+using WHampson.Cascara;
+using Xunit;
+
+namespace Cascara.Tests
+{
+    public class StatementTests
+    {
+        private Statement CreateXmlStatement(string xml)
+        {
+            XDocument doc = XDocument.Parse(xml, LoadOptions.SetLineInfo);
+
+            return new XmlStatement(doc.Root);
+        }
+
+        // TODO: make this cleaner and more readable! 
+        [Theory]
+        [InlineData("<int/>", "<int/>", true)]
+        [InlineData("<int/>", "<int name='foo'/>", false)]
+        [InlineData("<int count='4' name='foo'/>", "<int name='foo' count='4'/>", true)]
+        [InlineData("<int/>", "<char/>", false)]
+        [InlineData("<struct><float/></struct>", "<struct><float/></struct>", true)]
+        [InlineData("<struct><float/></struct>", "<struct><int/></struct>", false)]
+        [InlineData("<typedef name='my_t' kind='struct'><float name='foo' comment='bar'/></typedef>",
+            "<typedef kind='struct' name='my_t'><float comment='bar' name='foo'/></typedef>", true)]
+        [InlineData("<struct><int/><float/></struct>", "<struct><int/><float/></struct>", true)]
+        [InlineData("<struct><float/><float/></struct>", "<struct><int/><float/></struct>", false)]
+        [InlineData("<struct><int/><float/></struct>", "<struct><int name='foo'/><float/></struct>", false)]
+        public void TestEquality(string leftXml, string rightXml, bool expectedResult)
+        {
+            // Arrange
+            Statement stmtA = CreateXmlStatement(leftXml);
+            Statement stmtB = CreateXmlStatement(rightXml);
+
+            // Act
+            bool result1 = stmtA.Equals(stmtB);
+            bool result2 = stmtB.Equals(stmtA);
+
+            // Assert
+            Assert.Equal(result1, expectedResult);
+            Assert.Equal(result2, expectedResult);
+            if (expectedResult == true)
+            {
+                Assert.Equal(stmtA.GetHashCode(), stmtB.GetHashCode());
+            }
+
+        }
+    }
+}
---
+++ Cascara.Tests/Src/XmlStatementTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using WHampson.Cascara;
+using Xunit;
+
+namespace Cascara.Tests
+{
+    public class XmlStatementTests
+    {
+        private XElement CreateXmlElement(string data)
+        {
+            XDocument doc = XDocument.Parse(data, LoadOptions.SetLineInfo);
+
+            return doc.Root;
+        }
+
+        [Fact]
+        public void TestCreate()
+        {
+            // Arrange
+            string expectedKeyword = "foo";
+            bool expectedHasParameters = false;
+            int expectedParameterCount = 0;
+            bool expectedHasNestedStatements = false;
+            int expectedNestedStatementsCount = 0;
+            XElement elem = CreateXmlElement("<foo/>");
+
+            // Act
+            Statement stmt = new XmlStatement(elem);
+
+            // Assert
+            Assert.Equal(stmt.Keyword, expectedKeyword);
+            Assert.Equal(stmt.HasParameters, expectedHasParameters);
+            Assert.Equal(stmt.Parameters.Count, expectedParameterCount);
+            Assert.Equal(stmt.HasNestedStatements, expectedHasNestedStatements);
+            Assert.Equal(stmt.NestedStatements.Count(), expectedNestedStatementsCount);
+        }
+
+        [Fact]
+        public void TestCreateParametered()
+        {
+            // Arrange
+            string expectedKeyword = "int";
+            bool expectedHasParameters = true;
+            int expectedParameterCount = 2;
+            bool expectedHasNestedStatements = false;
+            int expectedNestedStatementsCount = 0;
+            Tuple<string, string> expectedParam1 = Tuple.Create("name", "foo");
+            Tuple<string, string> expectedParam2 = Tuple.Create("count", "5");
+            XElement elem = CreateXmlElement("<int name='foo' count='5'/>");
+
+            // Act
+            Statement stmt = new XmlStatement(elem);
+
+            // Assert
+            Assert.Equal(stmt.Keyword, expectedKeyword);
+            Assert.Equal(stmt.HasParameters, expectedHasParameters);
+            Assert.Equal(stmt.Parameters.Count, expectedParameterCount);
+            Assert.Equal(stmt.HasNestedStatements, expectedHasNestedStatements);
+            Assert.Equal(stmt.NestedStatements.Count(), expectedNestedStatementsCount);
+            Assert.Equal(stmt.Parameters[expectedParam1.Item1], expectedParam1.Item2);
+            Assert.Equal(stmt.Parameters[expectedParam2.Item1], expectedParam2.Item2);
+        }
+
+        [Fact]
+        public void TestCreateNested()
+        {
+            // Arrange
+            string expectedKeywordParent = "struct";
+            string[] expectedKeywordChild = { "int", "float" };
+            bool expectedHasParameters = false;
+            int expectedParameterCount = 0;
+            bool expectedHasNestedStatements = true;
+            int expectedNestedStatementsCount = 2;
+            XElement elem = CreateXmlElement("<struct><int/><float/></struct>");
+
+            // Act
+            Statement stmt = new XmlStatement(elem);
+
+            // Assert
+            Assert.Equal(stmt.Keyword, expectedKeywordParent);
+            Assert.Equal(stmt.HasParameters, expectedHasParameters);
+            Assert.Equal(stmt.Parameters.Count, expectedParameterCount);
+            Assert.Equal(stmt.HasNestedStatements, expectedHasNestedStatements);
+            Assert.Equal(stmt.NestedStatements.Count(), expectedNestedStatementsCount);
+
+            for (int i = 0; i < stmt.NestedStatements.Count(); i++)
+            {
+                Statement childStmt = stmt.NestedStatements.ElementAt(i);
+                Assert.Equal(childStmt.Keyword, expectedKeywordChild[i]);
+            }
+        }
+
+        [Fact]
+        public void TestCreateWithText()
+        {
+            // Arrange
+            XElement elem = CreateXmlElement("<string>Hello, world!</string>");
+
+            // Act, assert
+            var ex = Assert.Throws<SyntaxException>(() => new XmlStatement(elem));
+        }
+    }
+}
