423a759188c27b0857d03b9eb2e10f969d7d10b3
Wes Hampson <thehambone93@gmail.com>
2018-01-02 13:07:28 -0600

    Add static methods for building XML data strings

--- Cascara.Tests/Src/CascaraTestFramework.cs
+++ Cascara.Tests/Src/CascaraTestFramework.cs
@@ -1,9 +1,34 @@
-﻿using Xunit.Abstractions;
+﻿using System;
+using System.Xml.Linq;
+using Xunit.Abstractions;
 
 namespace Cascara.Tests
 {
     public abstract class CascaraTestFramework
     {
+        protected static string BuildXmlElement(string name, params Tuple<string, string>[] attrs)
+        {
+            return BuildXmlElement(name, "", attrs);
+        }
+
+        protected static string BuildXmlElement(string name, string innerData, params Tuple<string, string>[] attrs)
+        {
+            string attrString = "";
+            foreach (Tuple<string, string> t in attrs)
+            {
+                attrString += string.Format(" {0}='{1}'", t.Item1, t.Item2);
+            }
+
+            return string.Format("<{0}{1}>{2}</{0}>", name, attrString, innerData);
+        }
+
+        protected static XElement ParseXmlElement(string data)
+        {
+            XDocument doc = XDocument.Parse(data, LoadOptions.SetLineInfo);
+
+            return doc.Root;
+        }
+
         public CascaraTestFramework(ITestOutputHelper output)
         {
             Output = output;
