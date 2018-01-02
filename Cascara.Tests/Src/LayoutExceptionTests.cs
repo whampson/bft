e326eaad72eefd533bbce339e200551b6a8f9e5a
Wes Hampson <thehambone93@gmail.com>
2018-01-01 20:14:51 -0600

    Refactor to use ISourceElement

--- Cascara.Tests/Src/LayoutExceptionTests.cs
+++ Cascara.Tests/Src/LayoutExceptionTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Xml.Linq;
 using WHampson.Cascara;
+using WHampson.Cascara.Interpreter;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -18,7 +18,7 @@ namespace Cascara.Tests
         {
             // Arrange
             BinaryLayout layout = null;
-            XObject obj = null;
+            ISourceElement srcElem = null;
             string msg = null;
             Exception innerException = null;
 
@@ -29,7 +29,7 @@ namespace Cascara.Tests
             Exception expectedInnerException = null;
 
             // Act
-            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, obj, msg);
+            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, srcElem, msg);
 
             // Assert
             Assert.Equal(expectedMessage, ex.Message);
@@ -40,11 +40,11 @@ namespace Cascara.Tests
         }
 
         [Fact]
-        public void Create_Message()
+        public void Create_WithMessage()
         {
             // Arrange
             BinaryLayout layout = null;
-            XObject obj = null;
+            ISourceElement srcElem = null;
             string msg = "Test.";
             Exception innerException = null;
 
@@ -55,7 +55,7 @@ namespace Cascara.Tests
             Exception expectedInnerException = null;
 
             // Act
-            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, obj, msg);
+            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, srcElem, msg);
 
             // Assert
             Assert.Equal(expectedMessage, ex.Message);
@@ -66,11 +66,11 @@ namespace Cascara.Tests
         }
 
         [Fact]
-        public void Create_InnerExceptionWithMessage()
+        public void Create_NestedWithMessage()
         {
             // Inputs
             BinaryLayout layout = null;
-            XObject obj = null;
+            ISourceElement srcElem = null;
             string msg = "Failed to process layout.";
             Exception innerException = new ArgumentNullException("foo", "The parameter was null."); ;
 
@@ -85,7 +85,7 @@ Caused by:
             Exception expectedInnerException = innerException;
 
             // Execution
-            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, obj, msg);
+            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, srcElem, msg);
 
             // Assertion
             Assert.Equal(expectedMessage, ex.Message);
@@ -94,5 +94,35 @@ Caused by:
             Assert.Equal(expectedLinePos, ex.LinePosition);
             Assert.Equal(expectedInnerException, ex.InnerException);
         }
+
+//        [Fact]
+//        public void Create_NestedLayoutExceptionWithMessage()
+//        {
+//            // Inputs
+//            BinaryLayout layout = BinaryLayout.Create("<cascaraLayout><foo/></cascaraLayout>");
+//            ISourceElement srcElem = null;
+//            string msg = "Failed to process layout.";
+//            Exception innerException = new ArgumentNullException("foo", "The parameter was null."); ;
+
+//            // Expected results
+//            string expectedMessage = msg;
+//            string expectedDetailedMessage = msg + @"
+//Caused by:
+//  ArgumentNullException: The parameter was null.
+//    Parameter name: foo";
+//            int expectedLineNum = 0;
+//            int expectedLinePos = 0;
+//            Exception expectedInnerException = innerException;
+
+//            // Execution
+//            LayoutException ex = LayoutException.Create<LayoutException>(layout, innerException, srcElem, msg);
+
+//            // Assertion
+//            Assert.Equal(expectedMessage, ex.Message);
+//            Assert.Equal(expectedDetailedMessage, ex.DetailedMessage);
+//            Assert.Equal(expectedLineNum, ex.LineNumber);
+//            Assert.Equal(expectedLinePos, ex.LinePosition);
+//            Assert.Equal(expectedInnerException, ex.InnerException);
+//        }
     }
 }
