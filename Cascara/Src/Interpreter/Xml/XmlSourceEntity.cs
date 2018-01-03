20bd1d19df30964d0dfa85e1cbc0acba7e4f0a3d
Wes Hampson <thehambone93@gmail.com>
2018-01-02 18:31:50 -0600

    Add class 'XmlSourceEntity'

---
+++ Cascara/Src/Interpreter/Xml/XmlSourceEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using WHampson.Cascara.Extensions;
+
+namespace WHampson.Cascara.Interpreter.Xml
+{
+    /// <summary>
+    /// Encapsulates an <see cref="XObject"/> as an <see cref="ISourceEntity"/>.
+    /// </summary>
+    internal sealed class XmlSourceEntity : ISourceEntity
+    {
+        public XmlSourceEntity(XObject sourceObject)
+        {
+            if (sourceObject == null)
+            {
+                throw new ArgumentNullException(nameof(sourceObject));
+            }
+
+            SourceObject = sourceObject;
+        }
+
+        public XObject SourceObject
+        {
+            get;
+        }
+
+        int ISourceEntity.LineNumber
+        {
+            get { return SourceObject.GetLineInfo().Item1; }
+        }
+
+        int ISourceEntity.LinePosition
+        {
+            get { return SourceObject.GetLineInfo().Item2; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: [ {1} = {2} ]",
+                GetType().Name,
+                nameof(SourceObject), SourceObject);
+        }
+    }
+}
