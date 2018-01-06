af817fc0a0bb369336241e32d1feaf23991eca2d
Wes Hampson <thehambone93@gmail.com>
2018-01-06 14:25:46 -0600

    Add IsPrimitiveType() and support for Cascara primitives

--- Cascara/Src/PrimitiveTypeUtils.cs
+++ Cascara/Src/PrimitiveTypeUtils.cs
@@ -29,10 +29,24 @@ using System.Text;
 namespace WHampson.Cascara
 {
     /// <summary>
-    /// Utility class for functionality related to .NET primitive data types.
+    /// Utility class for functionality related to .NET and Cascara primitive data types.
     /// </summary>
+    /// <remarks>
+    /// Note that for the context this class library, <see cref="System.IntPtr"/> and
+    /// <see cref="System.UIntPtr"/> are not considered primitive types.
+    /// </remarks>
     internal static class PrimitiveTypeUtils
     {
+        public static bool IsPrimitiveType<T>()
+        {
+            return IsPrimitiveType(typeof(T));
+        }
+
+        public static bool IsPrimitiveType(Type t)
+        {
+            return PrimitiveTypes.Contains(t);
+        }
+
         /// <summary>
         /// Gets the size of a primitive type.
         /// </summary>
@@ -63,7 +77,7 @@ namespace WHampson.Cascara
         /// <returns>The size of the type in bytes.</returns>
         public static int SizeOf(Type t)
         {
-            if (!t.IsPrimitive)
+            if (!IsPrimitiveType(t))
             {
                 throw new ArgumentException(Resources.ArgumentExceptionPrimitiveType, nameof(t));
             }
@@ -91,7 +105,7 @@ namespace WHampson.Cascara
         /// <returns>An array containing the bytes represented by the specified value.</returns>
         public static byte[] GetBytes(Type t, object value)
         {
-            if (!t.IsPrimitive)
+            if (!IsPrimitiveType(t))
             {
                 throw new ArgumentException(Resources.ArgumentExceptionPrimitiveType, nameof(t));
             }
@@ -100,6 +114,7 @@ namespace WHampson.Cascara
                 throw new ArgumentException(Resources.ArgumentExceptionTypeMismatch, nameof(value));
             }
 
+            // .NET types
             if (t == typeof(bool)) return BitConverter.GetBytes((bool) value);
             if (t == typeof(byte)) return new byte[] { (byte) value };
             if (t == typeof(sbyte)) return new byte[] { (byte) ((sbyte) value) };
@@ -111,7 +126,16 @@ namespace WHampson.Cascara
             if (t == typeof(long)) return BitConverter.GetBytes((long) value);
             if (t == typeof(ulong)) return BitConverter.GetBytes((ulong) value);
             if (t == typeof(short)) return BitConverter.GetBytes((short) value);
-            return BitConverter.GetBytes((ushort) value);
+            if (t == typeof(ushort)) return BitConverter.GetBytes((ushort) value);
+
+            // Cascara types
+            if (t == typeof(Bool16)) return BitConverter.GetBytes((ushort) Convert.ChangeType(value, typeof(ushort)));
+            if (t == typeof(Bool32)) return BitConverter.GetBytes((uint) Convert.ChangeType(value, typeof(uint)));
+            if (t == typeof(Bool64)) return BitConverter.GetBytes((ulong) Convert.ChangeType(value, typeof(ulong)));
+            if (t == typeof(Char8)) return new byte[] { (byte) Convert.ChangeType(value, typeof(byte)) };
+
+            string msg = string.Format("Bug! Reached the end of {0}.{1}.", nameof(PrimitiveTypeUtils), nameof(GetBytes));
+            throw new InvalidOperationException(msg);
         }
 
         /// <summary>
@@ -134,7 +158,7 @@ namespace WHampson.Cascara
         /// <returns>The primitive type value represented by the bytes provided.</returns>
         public static object GetValue(Type t, byte[] b)
         {
-            if (!t.IsPrimitive)
+            if (!IsPrimitiveType(t))
             {
                 throw new ArgumentException(Resources.ArgumentExceptionPrimitiveType, nameof(t));
             }
@@ -143,6 +167,7 @@ namespace WHampson.Cascara
                 throw new ArgumentException(Resources.ArgumentExceptionEmptyCollection, nameof(b));
             }
 
+            // .NET types
             if (t == typeof(bool)) return BitConverter.ToBoolean(b, 0);
             if (t == typeof(byte)) return b[0];
             if (t == typeof(sbyte)) return (sbyte) b[0];
@@ -154,14 +179,50 @@ namespace WHampson.Cascara
             if (t == typeof(long)) return BitConverter.ToInt64(b, 0);
             if (t == typeof(ulong)) return BitConverter.ToUInt64(b, 0);
             if (t == typeof(short)) return BitConverter.ToInt16(b, 0);
-            return BitConverter.ToUInt16(b, 0);
+            if (t == typeof(ushort)) return BitConverter.ToUInt16(b, 0);
+
+            // Cascara types
+            if (t == typeof(Bool16)) return (Bool16) (BitConverter.ToUInt16(b, 0) != 0);
+            if (t == typeof(Bool32)) return (Bool32) (BitConverter.ToUInt32(b, 0) != 0);
+            if (t == typeof(Bool64)) return (Bool64) (BitConverter.ToUInt64(b, 0) != 0);
+            if (t == typeof(Char8)) return (Char8) ((char) Convert.ChangeType(b[0], typeof(char)));
+
+            string msg = string.Format("Bug! Reached the end of {0}.{1}.", nameof(PrimitiveTypeUtils), nameof(GetValue));
+            throw new InvalidOperationException(msg);
         }
+
+        /// <summary>
+        /// A lookup table of all primitive types supported by Cascara.
+        /// </summary>
+        private static readonly HashSet<Type> PrimitiveTypes = new HashSet<Type>()
+        {
+            // .NET types
+            typeof(bool),
+            typeof(byte),
+            typeof(sbyte),
+            typeof(char),
+            typeof(double),
+            typeof(float),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(short),
+            typeof(ushort),
+
+            // Cascara types
+            typeof(Bool16),
+            typeof(Bool32),
+            typeof(Bool64),
+            typeof(Char8)
+        };
 
         /// <summary>
         /// A lookup table to find the size of any .NET primitive type.
         /// </summary>
         private static readonly IReadOnlyDictionary<Type, int> SizeOfMap = new Dictionary<Type, int>()
         {
+            // .NET types
             { typeof(bool), sizeof(bool) },
             { typeof(byte), sizeof(byte) },
             { typeof(sbyte), sizeof(sbyte) },
@@ -173,7 +234,13 @@ namespace WHampson.Cascara
             { typeof(long), sizeof(long) },
             { typeof(ulong), sizeof(ulong) },
             { typeof(short), sizeof(short) },
-            { typeof(ushort), sizeof(ushort) }
+            { typeof(ushort), sizeof(ushort) },
+
+            // Cascara types
+            { typeof(Bool16), sizeof(ushort) },
+            { typeof(Bool32), sizeof(uint) },
+            { typeof(Bool64), sizeof(ulong) },
+            { typeof(Char8), sizeof(byte) },
         };
     }
 }
