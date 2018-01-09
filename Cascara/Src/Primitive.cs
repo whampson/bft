dbcb9accf6216168d6bec25a76fcfc2f63ca4453
Wes Hampson <thehambone93@gmail.com>
2018-01-08 23:28:13 -0600

    Documentation

--- Cascara/Src/Primitive.cs
+++ Cascara/Src/Primitive.cs
@@ -29,6 +29,10 @@ using WHampson.Cascara.Interpreter;
 
 namespace WHampson.Cascara
 {
+    /// <summary>
+    /// Represents a primitive data type found in a <see cref="BinaryFile"/>.
+    /// </summary>
+    /// <typeparam name="T">The .NET or Cascara type that this object encapsulates.</typeparam>
     public class Primitive<T> : IFileObject
         where T : struct
     {
@@ -52,22 +56,49 @@ namespace WHampson.Cascara
             this.symbol = symbol;
         }
 
+        /// <summary>
+        /// Gets the element of the collection at the specified index.
+        /// Throws <see cref="InvalidOperationException"/> if this <see cref="Primitive{T}"/>
+        /// does not represent a collection.
+        /// </summary>
+        /// <param name="index">The index of the element to get.</param>
+        /// <returns>The element at the specified index.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the specified index is negative or greater than or equal to the element count.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this <see cref="Primitive{T}"/> does not represent a collection.
+        /// </exception>
+        /// <seealso cref="IsCollection"/>
         public Primitive<T> this[int index]
         {
             get { return (Primitive<T>) ((IFileObject) this).ElementAt(index); }
         }
 
+        /// <summary>
+        /// Gets or sets the value of this primitive type.
+        /// Setting this property will change the bytes in the <see cref="BinaryFile"/>
+        /// at the location specified by <see cref="FilePosition"/>.
+        /// </summary>
         public T Value
         {
             get { return sourceFile.Get<T>(FilePosition); }
             set { sourceFile.Set<T>(FilePosition, value); }
         }
 
+        /// <summary>
+        /// Gets the position of this <see cref="IFileObject"/> relative to the start
+        /// of the <see cref="BinaryFile"/>.
+        /// </summary>
         public int FilePosition
         {
             get { return symbol.DataOffset; }
         }
 
+        /// <summary>
+        /// Gets the position of this <see cref="IFileObject"/> relative to the start
+        /// of the parent object.
+        /// </summary>
         public int Offset
         {
             get
@@ -81,21 +112,46 @@ namespace WHampson.Cascara
             }
         }
 
+        /// <summary>
+        /// Gets the number of bytes that make up this <see cref="IFileObject"/>.
+        /// </summary>
         public int Length
         {
             get { return symbol.DataLength; }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="IFileObject"/> represents a collection.
+        /// </summary>
         public bool IsCollection
         {
             get { return symbol.IsCollection; }
         }
 
+        /// <summary>
+        /// Gets the number of elements in the collection represented by this <see cref="IFileObject"/>.
+        /// If this <see cref="IFileObject"/> does not represent a collection, this value is -1.
+        /// </summary>
+        /// <seealso cref="IsCollection"/>
         public int ElementCount
         {
             get { return symbol.ElementCount; }
         }
 
+        /// <summary>
+        /// Gets the element at the specified index in the collection as an <see cref="IFileObject"/>.
+        /// If this <see cref="IFileObject"/> does not represent a collection, an
+        /// <see cref="InvalidOperationException"/> will be thrown.
+        /// </summary>
+        /// <param name="index">The index of the element to get.</param>
+        /// <returns>The element at the specified index.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the specified index is negative or greater than or equal to the element count.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this <see cref="IFileObject"/> does not represent a collection.
+        /// </exception>
+        /// <seealso cref="IsCollection"/>
         IFileObject IFileObject.ElementAt(int index)
         {
             if (!IsCollection)
@@ -130,6 +186,11 @@ namespace WHampson.Cascara
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Converts this <see cref="Primitive{T}"/> object to an <see cref="int"/>
+        /// whose value equals <see cref="FilePosition"/>.
+        /// </summary>
+        /// <param name="p">The <see cref="Primitive{T}"/> object to convert to an <see cref="int"/>.</param>
         public static implicit operator int(Primitive<T> p)
         {
             return p.FilePosition;
--- Cascara/Src/Structure.cs
+++ Cascara/Src/Structure.cs
@@ -7,6 +7,9 @@ using WHampson.Cascara.Interpreter;
 
 namespace WHampson.Cascara
 {
+    /// <summary>
+    /// Represents a composite data type found in a <see cref="BinaryFile"/>.
+    /// </summary>
     public class Structure : IFileObject
     {
         private BinaryFile sourceFile;
@@ -18,16 +21,38 @@ namespace WHampson.Cascara
             this.symbol = symbol;
         }
 
+        /// <summary>
+        /// Gets the element of the collection at the specified index.
+        /// Throws <see cref="InvalidOperationException"/> if this <see cref="Structure"/>
+        /// does not represent a collection.
+        /// </summary>
+        /// <param name="index">The index of the element to get.</param>
+        /// <returns>The element at the specified index.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the specified index is negative or greater than or equal to the element count.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this <see cref="Structure"/> does not represent a collection.
+        /// </exception>
+        /// <seealso cref="IsCollection"/>
         public Structure this[int index]
         {
             get { return (Structure) ((IFileObject) this).ElementAt(index); }
         }
 
+        /// <summary>
+        /// Gets the position of this <see cref="IFileObject"/> relative to the start
+        /// of the <see cref="BinaryFile"/>.
+        /// </summary>
         public int FilePosition
         {
             get { return symbol.DataOffset; }
         }
 
+        /// <summary>
+        /// Gets the position of this <see cref="IFileObject"/> relative to the start
+        /// of the parent object.
+        /// </summary>
         public int Offset
         {
             get
@@ -41,54 +66,72 @@ namespace WHampson.Cascara
             }
         }
 
+        /// <summary>
+        /// Gets the number of bytes that make up this <see cref="IFileObject"/>.
+        /// </summary>
         public int Length
         {
             get { return symbol.DataLength; }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="IFileObject"/> represents a collection.
+        /// </summary>
         public bool IsCollection
         {
             get { return symbol.IsCollection; }
         }
 
+        /// <summary>
+        /// Gets the number of elements in the collection represented by this <see cref="IFileObject"/>.
+        /// If this <see cref="IFileObject"/> does not represent a collection, this value is -1.
+        /// </summary>
+        /// <seealso cref="IsCollection"/>
         public int ElementCount
         {
             get { return symbol.ElementCount; }
         }
 
-        public int SizeOf(string name)
-        {
-            bool exists = symbol.TryLookup(name, out Symbol sym);
-            if (!exists)
-            {
-                return 0;
-            }
+        //public int SizeOf(string name)
+        //{
+        //    bool exists = symbol.TryLookup(name, out Symbol sym);
+        //    if (!exists)
+        //    {
+        //        return 0;
+        //    }
 
-            return sym.DataLength;
-        }
+        //    return sym.DataLength;
+        //}
 
-        public Type TypeOf(string name)
-        {
-            bool exists = symbol.TryLookup(name, out Symbol sym);
-            if (!exists)
-            {
-                return null;
-            }
+        //public Type TypeOf(string name)
+        //{
+        //    bool exists = symbol.TryLookup(name, out Symbol sym);
+        //    if (!exists)
+        //    {
+        //        return null;
+        //    }
 
-            return sym.DataType;
-        }
+        //    return sym.DataType;
+        //}
 
-        public int OffsetOf(string name)
-        {
-            bool exists = symbol.TryLookup(name, out Symbol sym);
-            if (!exists)
-            {
-                return -1;
-            }
+        //public int OffsetOf(string name)
+        //{
+        //    bool exists = symbol.TryLookup(name, out Symbol sym);
+        //    if (!exists)
+        //    {
+        //        return -1;
+        //    }
 
-            return sym.DataOffset;
-        }
+        //    return sym.DataOffset;
+        //}
 
+        /// <summary>
+        /// Gets a <see cref="Structure"/> by searching this structure's
+        /// symbol table for the specified name. If no match is found,
+        /// <c>null</c> is returned.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="Structure"/> to search for.</param>
+        /// <returns>The <see cref="Structure"/> object, if found. <c>null</c> otherwise</returns>
         public Structure GetStructure(string name)
         {
             bool exists = symbol.TryLookup(name, out Symbol sym);
@@ -97,9 +140,19 @@ namespace WHampson.Cascara
                 return null;
             }
 
+            // TODO: ensure symbol referrs to a structure (not a primitive)
+
             return new Structure(sourceFile, sym);
         }
 
+        /// <summary>
+        /// Gets a <see cref="Primitive{T}"/> by searching this structure's
+        /// symbol table for the specified name. If no match is found,
+        /// <c>null</c> is returned.
+        /// </summary>
+        /// <typeparam name="T">The type of primitive to get.</typeparam>
+        /// <param name="name">The name of the <see cref="Primitive{T}"/> to search for.</param>
+        /// <returns>The <see cref="Primitive{T}"/> object, if found. <c>null</c> otherwise</returns>
         public Primitive<T> GetPrimitive<T>(string name)
             where T : struct
         {
@@ -109,9 +162,25 @@ namespace WHampson.Cascara
                 return null;
             }
 
+            // TODO: ensure symbol referrs to a primitive (not a structure)
+
             return new Primitive<T>(sourceFile, sym);
         }
 
+        /// <summary>
+        /// Gets the element at the specified index in the collection as an <see cref="IFileObject"/>.
+        /// If this <see cref="IFileObject"/> does not represent a collection, an
+        /// <see cref="InvalidOperationException"/> will be thrown.
+        /// </summary>
+        /// <param name="index">The index of the element to get.</param>
+        /// <returns>The element at the specified index.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the specified index is negative or greater than or equal to the element count.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this <see cref="IFileObject"/> does not represent a collection.
+        /// </exception>
+        /// <seealso cref="IsCollection"/>
         IFileObject IFileObject.ElementAt(int index)
         {
             if (!IsCollection)
@@ -151,47 +220,52 @@ namespace WHampson.Cascara
             throw new NotImplementedException();
         }
 
-        public Structure AddStructure(string name, int offset, int length)
-        {
-            Symbol sym = symbol.Insert(name);
-            sym.DataOffset = offset;
-            sym.DataLength = length;
+        //public Structure AddStructure(string name, int offset, int length)
+        //{
+        //    Symbol sym = symbol.Insert(name);
+        //    sym.DataOffset = offset;
+        //    sym.DataLength = length;
 
-            return (Structure) GetStructure(name);
-        }
+        //    return (Structure) GetStructure(name);
+        //}
 
-        public Primitive<T> AddPrimitive<T>(string name, int offset)
-            where T : struct
-        {
-            Symbol sym = symbol.Insert(name);
-            sym.DataOffset = offset;
-            sym.DataLength = PrimitiveTypeUtils.SizeOf<T>();
-            sym.DataType = typeof(T);
+        //public Primitive<T> AddPrimitive<T>(string name, int offset)
+        //    where T : struct
+        //{
+        //    Symbol sym = symbol.Insert(name);
+        //    sym.DataOffset = offset;
+        //    sym.DataLength = PrimitiveTypeUtils.SizeOf<T>();
+        //    sym.DataType = typeof(T);
 
-            return GetPrimitive<T>(name);
-        }
+        //    return GetPrimitive<T>(name);
+        //}
 
-        public Primitive<T> AddPrimitive<T>(string name, int offset, int count)
-            where T : struct
-        {
-            int typeSize = PrimitiveTypeUtils.SizeOf<T>();
+        //public Primitive<T> AddPrimitive<T>(string name, int offset, int count)
+        //    where T : struct
+        //{
+        //    int typeSize = PrimitiveTypeUtils.SizeOf<T>();
 
-            Symbol sym = symbol.Insert(name, count);
-            sym.DataOffset = offset;
-            sym.DataLength = typeSize * count;
-            sym.DataType = typeof(T[]);
+        //    Symbol sym = symbol.Insert(name, count);
+        //    sym.DataOffset = offset;
+        //    sym.DataLength = typeSize * count;
+        //    sym.DataType = typeof(T[]);
 
-            int i = 0;
-            foreach (Symbol elem in sym)
-            {
-                elem.DataOffset = sym.DataOffset + (i * typeSize);
-                elem.DataLength = typeSize;
-                i++;
-            }
+        //    int i = 0;
+        //    foreach (Symbol elem in sym)
+        //    {
+        //        elem.DataOffset = sym.DataOffset + (i * typeSize);
+        //        elem.DataLength = typeSize;
+        //        i++;
+        //    }
 
-            return GetPrimitive<T>(name);
-        }
+        //    return GetPrimitive<T>(name);
+        //}
 
+        /// <summary>
+        /// Converts this <see cref="Structure"/> object to an <see cref="int"/>
+        /// whose value equals <see cref="FilePosition"/>.
+        /// </summary>
+        /// <param name="p">The <see cref="Structure"/> object to convert to an <see cref="int"/>.</param>
         public static implicit operator int(Structure s)
         {
             return s.FilePosition;
